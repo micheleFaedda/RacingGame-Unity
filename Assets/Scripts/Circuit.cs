c6f4ac4aba2fde9a45af5001837ad78f02179175
tvs111 <tvs034@gmail.com>
2022-02-26 23:55:35 +0100

    seconda curva -> brutto barbaro

--- Assets/Scripts/Circuit.cs
+++ Assets/Scripts/Circuit.cs
@@ -1,35 +1,52 @@
 ﻿using UnityEngine;
 
-public class Circuit : MonoBehaviour {
-    
+public class Circuit : MonoBehaviour
+{
 
-    public GameObject[] waypoints;
 
-    private void OnDrawGizmos() {
+    //public GameObject[] waypoints;
+    public Transform[] waypoints;
+
+
+    private void Start()
+    {
+        waypoints = new Transform[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            waypoints[i] = transform.GetChild(i);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
 
         DrawGizmos(false);
     }
 
-    private void OnDrawGizmosSelected() {
+    private void OnDrawGizmosSelected()
+    {
 
         DrawGizmos(true);
     }
 
-    void DrawGizmos(bool selected) {
+    void DrawGizmos(bool selected)
+    {
 
         if (selected == false) return;
 
-        if (waypoints.Length > 1) {
+        if (waypoints.Length > 1)
+        {
 
-            Vector3 prev = waypoints[0].transform.position;
-            for (int i = 1; i < waypoints.Length; ++i) {
+            Vector3 prev = waypoints[0].position;
+            for (int i = 1; i < waypoints.Length; ++i)
+            {
 
-                Vector3 next = waypoints[i].transform.position;
+                Vector3 next = waypoints[i].position;
                 Gizmos.DrawLine(prev, next);
                 prev = next;
             }
-            Gizmos.DrawLine(prev, waypoints[0].transform.position);
+            Gizmos.DrawLine(prev, waypoints[0].position);
         }
     }
-    
+
 }
