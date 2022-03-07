5373cd4ce7154c18539fd1bd9a9e496530a6820b
tvs111 <tvs034@gmail.com>
2022-03-07 23:38:04 +0100

    npc controller

--- Assets/Scripts/NPController.cs
+++ Assets/Scripts/NPController.cs
@@ -10,11 +10,13 @@ public class NPController : MonoBehaviour
 {
     
     private CarController carController;
+    
+    public float steeringSensitivity = 0.01f;
 
     //Posizione del wayPoint che la macchina non ha ancora raggiunto
     public Vector3 targetSucc;
     
-    //Posizione dell'ultimo wayPoint che la macchina non ha raggiunto (serve per la coroutine)
+    //Posizione dell'ultimo wayPoint che la macchina non ha raggiunto (serve solo per la coroutine)
     public Vector3 targeSucctOld;
 
     //Indice del wayPoint che la macchina non ha ancora raggiunto
@@ -31,8 +33,6 @@ public class NPController : MonoBehaviour
     
     void Start()
     {    
-        carController = this.GetComponent<CarController>();
-        
         //Andiamo a popolare l'array di waypoints
         waypoints = new Transform[circuito.transform.childCount];
         for (int i = 0; i < circuito.transform.childCount; i++)
@@ -40,55 +40,53 @@ public class NPController : MonoBehaviour
             waypoints[i] = circuito.transform.GetChild(i);
         }
         
+        carController = this.GetComponent<CarController>();
+        
         //Inizialmente il targetSucc è il primo nell'array
         targetSucc = waypoints[wpDaRaggiungere].transform.position;
         
-        //Start della coroutive che si occupa di correggere gli NPC
-        corutine = Riposizionamento(5f);
+        //Start della coroutine che si occupa di correggere gli NPC
+        corutine = WaitAndRepositioning(5f);
         StartCoroutine(corutine);
     }
 
     void Update()
     {
-        
-        //rb è il centro
+
         Vector3 localTarget = carController.rb.gameObject.transform.InverseTransformPoint(targetSucc);
 
-        //Distanza della macchina dal WP che deve raggiungere
-        float distanza = Vector3.Distance(targetSucc, carController.rb.transform.position);
+        float distanceToTarget = Vector3.Distance(targetSucc, carController.rb.transform.position);
 
-        //Atan2 restituisce radianti, convertiamo in gradi con Rad2Deg  
         float targetAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
 
         //blocco il valore di starzata tra -1 e 1
-        float sterzata = Mathf.Clamp(targetAngle * 0.01f, -1.0f, 1.0f) * Mathf.Sign(carController.VelocitaCorrente());
+        float steer = Mathf.Clamp(targetAngle * steeringSensitivity, -1.0f, 1.0f) * Mathf.Sign(carController.VelocitaCorrente());
         
         //Nel caso stia per raggiungere il waypoint accelerazione e frenata cambiano nell'if successivo
-        float accelerazione = 1f;
-        float frenata = 0;
+        float accel = 1f;
+        float brake = 0;
 
         //Nel caso stia per raggiungere il waypoint si frena per non mancare quello successivo
-        if(distanza < 5){
-            frenata = 0.8f;
-            accelerazione = 0.1f;
+        if(distanceToTarget < 5){
+            brake = 0.8f;
+            accel = 0.1f;
         }
         
+        carController.Move(accel, steer, brake);
+
         //Due if separati per non far frenare le macchine troppo tardi
-        
         //NPC non deve per forza prendere il wapoint in modo preciso, basta che si aviccii abastanza
-        if(distanza < 4){
+        if(distanceToTarget < 4){
             wpDaRaggiungere++;
             
             //Nel caso abbia fatto un ggiro completo il target diventa di nuovo il primo waypoint
-            if (wpDaRaggiungere >= waypoints.Length)
-                wpDaRaggiungere = 0;
-
+            if(wpDaRaggiungere >= waypoints.Length)
+                wpDaRaggiungere=0;
+            
             //Setto la posizione del prossimo wp da raggiungere
             targetSucc = waypoints[wpDaRaggiungere].transform.position;
         }
         
-        //La macchina viene mossa dal CarController
-        carController.Move(accelerazione, sterzata, frenata);
         carController.CheckSgommata();
         carController.SuonoMotore();
     }
@@ -97,7 +95,7 @@ public class NPController : MonoBehaviour
      * Si occupa di riposizionare gli NPC dopo un certo tempo nel caso questi non siano riusciti a raggiungere
      * un waypoint
      */
-    private IEnumerator Riposizionamento(float t)
+    private IEnumerator WaitAndRepositioning(float t)
     {
         while (true)
         {
@@ -111,6 +109,7 @@ public class NPController : MonoBehaviour
                 //Per riposizionare la macchina non basta settare la posizione, serve anche la rotazione per metterlo nella direzone giusta
                 this.transform.position =  waypoints[wpDaRaggiungere-1].transform.position + Vector3.up * 1.2f;
                 this.transform.rotation = waypoints[wpDaRaggiungere-1].transform.rotation;
+                //targeSucctOld = waypoints[wpDaRaggiungere-1].transform.position;
             }
         }
     }
