627fcde2c72abcf1fe829cef6e353711d7df548f
tvs111 <tvs034@gmail.com>
2022-03-14 10:45:26 +0100

    nnn

--- Assets/Scripts/CreateAndJoinRooms.cs
+++ Assets/Scripts/CreateAndJoinRooms.cs
@@ -1,25 +1,54 @@
+using System;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
-    //public InputField createInput;
-    //public InputField joinInput;
+    public TMP_InputField createInput;
+    public TMP_InputField joinInput;
 
+    public void BackButton()
+    {
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.Disconnect();
+        }
+
+        SceneManager.LoadScene("SceltaModalita");
+    }
+    
     public void CreateRoom(){
-        //PhotonNetwork.CreateRoom(createInput.text);
-        PhotonNetwork.CreateRoom("bb", new RoomOptions { MaxPlayers = 2 }, TypedLobby.Default);
+        PhotonNetwork.CreateRoom(createInput.text, new RoomOptions { MaxPlayers = 2 }, TypedLobby.Default);
     }
 
     public void JoinRoom(){
-        //PhotonNetwork.JoinRoom(joinInput.text);
-        PhotonNetwork.JoinRoom("bb");
+        PhotonNetwork.JoinRoom(joinInput.text);
     }
 
     public override void OnJoinedRoom(){
         PhotonNetwork.LoadLevel("Game");
     }
+    
+    /*Metodo che si occupa di settare il bottone corretto nel caso in cui ci sia o meno la stanza attivata
+    void SetButtons()
+    {
+        if (PhotonNetwork.CountOfPlayersInRooms > 0) //se vi sono player allora la stanza è aperta altrimenti no
+        {
+            buttonCreate.SetActive(false);
+            buttonJoin.SetActive(true);
+            buttonJoin.GetComponent<RectTransform>().localPosition = new Vector3(-631f, 43f, 0f);
+        }
+        else
+        {
+            buttonJoin.SetActive(false);
+            buttonCreate.SetActive(true);
+            buttonCreate.GetComponent<RectTransform>().localPosition = new Vector3(-631f, 43f, 0f);
+
+        }
+    }*/
 }
