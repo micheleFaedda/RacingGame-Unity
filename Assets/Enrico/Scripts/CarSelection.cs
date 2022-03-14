fa32d875e8fb52c7d8973af0d33194280db4dbc5
micheleFaedda <michele.faeddal@hotmail.com>
2022-03-14 08:10:27 +0100

    Merge enrico

--- Assets/Enrico/Scripts/CarSelection.cs
+++ Assets/Enrico/Scripts/CarSelection.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,25 +12,18 @@ public class CarSelection : MonoBehaviour
     private int currentCar;
     public string race;
     public string time;
-    public GameObject chooseModeScreen;
-    public Button goToChooseMode;
-    public Button unlockCar;
-    public Text coins;
-    private int numMacchine = 3;
+    public GameObject chooseCarScreen;
 
     private void Start()
     {
-        PlayerPrefs.SetInt("0", 1);
-        SelectCar(0);
-        coins.text = PlayerPrefs.GetInt("Coins").ToString();
-        unlockCar.interactable = false;
-
+        currentCar = SaveManager.instance.currentCar;
+        SelectCar(currentCar);
     }
 
 
     public void GoToChooseMode()
     {
-        chooseModeScreen.SetActive(true);
+        chooseCarScreen.SetActive(true);
     }
     public void StartRaceGame()
     {
@@ -45,70 +37,24 @@ public class CarSelection : MonoBehaviour
     private void SelectCar(int _index)
     {
         previousButton.interactable = (_index != 0);
-        nextButton.interactable = (_index != (transform.childCount)/2 - 1);
-        
+        nextButton.interactable = (_index != transform.childCount - 1);
 
-        for (int i = 0; i < (transform.childCount)/2; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
-
-            
-            if(PlayerPrefs.GetInt(i.ToString(), 0) == 0)
-            {
-                
-                transform.GetChild(i+numMacchine).gameObject.SetActive(i == _index);
-                
-                
-            }
-            else
-            {
-                
-                transform.GetChild(i).gameObject.SetActive(i == _index);
-                
-               
-            }
-            
+            transform.GetChild(i).gameObject.SetActive(i == _index);
         }
-
-
-
     }
 
     public void ChangeCar(int _change)
     {
         currentCar += _change;
-        if (currentCar > (transform.childCount)/2 - 1)
+        if (currentCar > transform.childCount - 1)
             currentCar = 0;
         else if (currentCar < 0)
             currentCar = transform.childCount - 1;
-        
+
         SaveManager.instance.currentCar = currentCar;
         SaveManager.instance.Save();
-        if(PlayerPrefs.GetInt(currentCar.ToString(),0) == 0)
-        {
-            goToChooseMode.interactable = false;
-            unlockCar.interactable = true;
-        }
-        else
-        {
-            goToChooseMode.interactable = true;
-            unlockCar.interactable = false;
-        }
         SelectCar(currentCar);
-
-    }
-
-    public void UnlockCar()
-    {
-        if (int.Parse(coins.text) >= 0)
-        {
-            PlayerPrefs.SetInt(currentCar.ToString(), 1);
-            transform.GetChild(currentCar + numMacchine).gameObject.SetActive(false);
-            SelectCar(currentCar);
-        }
-        else
-        {
-            Debug.Log("Non hai i soldi");
-        }
-        
     }
 }
